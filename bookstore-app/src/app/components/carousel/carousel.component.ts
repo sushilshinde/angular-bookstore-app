572769a64c0ca0c336c7ca00b43c1b5341ede852
Balajin19 <bn02149@gmail.com>
2023-08-03 12:28:29 +0530

    updated homepage

--- bookstore-app/src/app/components/carousel/carousel.component.ts
+++ bookstore-app/src/app/components/carousel/carousel.component.ts
@@ -10,19 +10,19 @@ export class CarouselComponent {
   // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   content: any[] = [
     {
-      imgUrl: 'https://images.booksamillion.com/home_flash/flashbanner_1/content/map/home/230703_AThousandBoyKisses_1200x440_v2.jpg',
+      imgUrl: '../../../assets/images/carousel images/carousel-1.jpg',
       title: 'A Thousand Boy Kisses',
     },  
     {
-      imgUrl: 'https://images.booksamillion.com/home_flash/flashbanner_1/content/map/home/230628_DivineRivals_1200x440_v1-1.jpg',
+      imgUrl: '../../../assets/images/carousel images/carousel-2.jpg',
       title: 'Divine Rivals',
     },
     {
-      imgUrl: 'https://images.booksamillion.com/home_flash/flashbanner_1/content/map/home/230713_BakingYesteryear_1200x440_v2.jpg',
+      imgUrl: '../../../assets/images/carousel images/carousel-3.jpg',
       title: 'Baking Yesteryear : The Best Recipes from the 1900s to the 1980s',
     },
     {
-      imgUrl: 'https://images.booksamillion.com/home_flash/flashbanner_1/content/map/home/230717_LightBringer_1200x440.jpg',
+      imgUrl: '../../../assets/images/carousel images/carousel-4.jpg',
       title: 'Light Bringer - A Red Rising Novel',
     },
 
