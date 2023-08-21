00c2bb22a091bc8df4bb29e07c018ac66f6ef4d0
srikanthmmsys <msveerabramham@msystechnologies.com>
2023-08-21 17:12:39 +0530

    auth service path changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatIconModule } from '@angular/material/icon';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
